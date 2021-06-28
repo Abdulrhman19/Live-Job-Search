24ca1d044fb08a1b17ca47f1b3f666aa1f10810f
Abdulrhman19 <am_alharthi19@hotmail.com>
2021-06-28 23:11:56 +0300

    Fetching from data.json and display it on job tiles

--- js/script.js
+++ js/script.js
@@ -3,3 +3,53 @@ document.querySelector(".button-container")
     let text = document.getElementById("filterJobs").value;
 
 });
+
+
+function getJobs() {
+    // fetch: is a way to get from API
+    return fetch("data.json")
+    .then(response => response.json())
+    .then(data => {
+        return data;
+    })
+}
+
+
+function showJobs(jobs) {
+    console.log(jobs);
+    let jobsContainer = document.querySelector(".jobs-container")
+
+    let jobsHTML = "";
+    jobs.forEach(job => {
+    
+        jobsHTML += `
+        <div class="job-tile">
+        <div class="top">
+          <img
+            src="${job.logo}"
+            alt=""
+          />
+          <span class="material-icons more_horiz"> more_horiz </span>
+        </div>
+        <div class="rolename">
+          <span>${job.roleName}</span>
+        </div>
+        <div class="description">
+          <span
+            >${job.requirements.content}</span
+          >
+        </div>
+        <div class="buttons">
+          <div class="button apply-now">Apply Now</div>
+          <div class="button">Message</div>
+        </div>
+      </div>
+        `;
+
+    });
+    jobsContainer.innerHTML = jobsHTML;
+}
+
+getJobs().then(data => {
+    showJobs(data);
+});
